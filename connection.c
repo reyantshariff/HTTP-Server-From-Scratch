8e9d4516c9af67fd3892645ad004906f878acdd6
Reyan Shariff <rts34@duke.edu>
2026-03-10 13:35:12 -0400

    Finish TCP Layer

--- connection.c
+++ connection.c
@@ -1,8 +1,11 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
     int sockfd;
-    struct sockaddr_in server_addr;
-
     // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0); //IPV4, TCP, 0 for default protocol (OS picks)
     
@@ -12,6 +15,32 @@ int main() {
         return 1;
     }
 
-    
+    // Set up server address structure
+    struct sockaddr_in server_addr={
+        .sin_family = AF_INET, //IPV4
+        .sin_port = htons(8080), //Port number (convert to network byte order)
+        .sin_addr.s_addr = INADDR_ANY,
+    };
 
+    int opt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+    int backlog=10; //Number of pending connections queue will hold
+    if (listen(sockfd, backlog) < 0) {
+        perror("listen");
+        return 1;
+    }
+    // Accept a connection (blocking call)
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+
+    if (clientfd < 0) {
+        perror("accept");
+        return 1;
+    }
 }
